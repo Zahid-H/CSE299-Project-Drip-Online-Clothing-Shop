bf3001590652cc774ee8897a59fe3381387f2eed
Zahid Hasan Juel <83463788+Zahid-H@users.noreply.github.com>
2023-03-19 14:48:08 +0600

    use Illuminate\Support\Facades\Schema; and Schema::defaultStringLength(191);  for table creation error

--- ecommerce/database/migrations/2023_03_19_082054_create_products_table.php
+++ ecommerce/database/migrations/2023_03_19_082054_create_products_table.php
@@ -4,7 +4,6 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
-
 return new class extends Migration
 {
     /**
@@ -14,12 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->bigInteger('category_id')->unsigned()->nullable();
+            $table->bigInteger('category_id')->unsigned();
             $table->foreign('category_id')->references('id')->on('categories')->onUpdate('cascade')->onDelete('cascade');
             $table->string('name');
-
+            $table->string('price');
+            $table->string('image');
             $table->enum('status',['1', '0'])->default('1');
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
@@ -33,7 +33,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
-
+        Schema::dropIfExists('products');
     }
 };
